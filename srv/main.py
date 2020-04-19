666510bc5b0dc88cf62d5c6ed67a96f44476deaa
Leigh Wilson <code-tap@leighwilson.xyz>
2020-04-19 09:11:31 +0100

    ready for testing

--- srv/main.py
+++ srv/main.py
@@ -4,28 +4,59 @@ import json
 from datetime import datetime
 from time import sleep
 
-def date_bin(currdate, currfulldate);
+def date_bin(currdate, currfulldate):
     # TODO : Parse json for data
-    fourletterphat.print_str(f"{num}{bins}")
-    fourletterphat.show()
+    firstLetter = ''
+    secondLetter = ''
+    match = False
 
-def weekday(currday);
+    try:
+        data = openJSON()
+    except:
+        break
+
+    for i in data:
+...     if i['bin'] == 'Grey Bin (General waste)':
+            if i['date'] == currfulldate:
+                firstLetter = 'G'
+                match = True
+...             # print(i['bin'], '==', i['day'], '==', i['date'])
+    for i in data:
+...     if i['bin'] == '240Ltr Green Bin (Recycling)':
+            if i['date'] == currfulldate:
+                secondLetter = 'G'
+                match = True
+
+    for i in data:
+...     if i['bin'] == '240Ltr Brown Bin (Garden waste)':
+            if i['date'] == currfulldate:
+                secondLetter = 'B'
+                match = True
+    
+    binletters = f"{firstLetter}{secondLetter}"
+    fourletterphat.print_str(f"{currdate}{binletters}")
+    if match:
+        fourletterphat.show()
+        binday = True
+    else:
+        binday = False
+
+def weekday(currday):
     scrollphat.write_string(f"{currday}", 0)
     
-def clearHats();
+def clearHats():
     scrollphat.clear()
     fourletterphat.clear()
 
-def dimHats();
+def dimHats():
     fourletterphat.set_brightness(0)
     scrollphat.set_brightness(1)
 
-def brightenHats();
+def brightenHats():
     fourletterphat.set_brightness(15)
     scrollphat.set_brightness(15)
 
-def openJSON();
-    data = ''
+def openJSON():
     with open("../binDates.json", "r") as read_file:
         data = json.load(read_file)
     return data
@@ -34,7 +65,7 @@ def openJSON();
 prevday = datetime.today().weekday()
 delta_hour = 0
 binday = false
-#data = openJSON()
+
 
 weekdays = {
         0:"Mo",
